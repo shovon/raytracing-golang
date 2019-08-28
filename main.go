9433e53553b0e1fbbafe0a60335b07c6dba4bad2
Sal Rahman <salehen.rahman@gmail.com>
2019-08-28 08:01:55 -0700

    Got a solid sphere showing.

--- main.go
+++ main.go
@@ -5,24 +5,21 @@ import (
 	"os"
 )
 
+func didHitSphere(center Vec3, radius float32, r Ray) bool {
+	oc := r.Origin().Subtract(center)
+	a := r.Direction().Dot(r.Direction())
+	b := 2.0 * oc.Dot(r.Direction())
+	c := oc.Dot(oc) - radius * radius
+	discriminant := b*b - 4*a*c
+	return discriminant > 0
+}
+
 func drawPixel(col Vec3) {
 	ir := int(255.99 * col.R())
 	ig := int(255.99 * col.G())
 	ib := int(255.99 * col.B())
 	fmt.Fprintf(os.Stdout, "%d %d %d\n", ir, ig, ib)
 }
-
-// func main() {
-// 	nx, ny := 200, 100
-
-// 	fmt.Fprintf(os.Stdout, "P3\n%d %d\n255\n", nx, ny)
-// 	for j := ny - 1; j >= 0; j-- {
-// 		for i := 0; i < nx; i++ {
-// 			col := Vec3{float32(i) / float32(nx), float32(j) / float32(ny), 0.2}
-// 			drawPixel(col)
-// 		}
-// 	}
-// }
 
 func main() {
 	nx, ny := 200, 100
@@ -51,6 +48,9 @@ func main() {
 }
 
 func color(r Ray) Vec3 {
+	if didHitSphere(Vec3{0.0, 0.0, -1.0}, 0.5, r) {
+		return Vec3{1.0, 0.0, 0.0}
+	}
 	unitDirection := r.Direction().Normalized()
 	t := 0.5 * unitDirection.Y() + 1.0
 	return Vec3{1.0, 1.0, 1.0}.ScalarMultiply(1.0 - t).
